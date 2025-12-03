054d541caacac06eb79a92daa4ab1557d26ca6a2
malik31200 <yka-vtc@outlook.fr>
2025-12-03 10:05:11 +0100

    variable

---
+++ coursPHP/maPage.php
@@ -0,0 +1,11 @@
+<?php
+
+$nomDuJoueur1 = "Likma";
+$age = 30;
+echo "Nom Du Joueur 1 : " . $nomDuJoueur1;
+echo "<br />";
+echo "age Du Joueur 1 : " . $age;
+echo "<br />";
+$age = $age +1 ;
+echo "age Du Joueur 1 : " . $age;
+?>
