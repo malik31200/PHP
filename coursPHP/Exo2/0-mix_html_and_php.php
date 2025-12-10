d2a949c6aafc077ce40b403eeb14185a7f8120e4
malik31200 <yka-vtc@outlook.fr>
2025-12-10 12:23:36 +0100

    differents style for 1 html file

---
+++ coursPHP/Exo2/0-mix_html_and_php.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Mon premier site</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="style.css" />
+    <script src="main.js"></script>
+    <!-- <style>
+        h1 {
+            color: red;
+        }
+        #titrebleu {
+            color: blue;    
+        }
+        /* 3facons de mettre du style */
+        .titrejaune {
+            color: yellow;
+        }
+    </style> -->
+</head>
+<body>
+    <!-- <h1> Salut </h1>
+    <h1 id="titrebleu"> Coucou </h1> 
+    <h1 class="titrejaune"> Aurevoir </h1> -->
+    <h1 class="titrejaune"> Salut </h1>
+    <h1 class="titrerouge"> Coucou!!! </h1>
+</body>
