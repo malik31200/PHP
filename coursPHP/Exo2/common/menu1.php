27c8b143c528b6a9e0a0e31a90e6053a65efa03f
malik31200 <yka-vtc@outlook.fr>
2025-12-11 12:24:05 +0100

    create a menu with footer and header

---
+++ coursPHP/Exo2/common/menu1.php
@@ -0,0 +1,8 @@
+<nav>
+    <ul>
+        <li><a href="menu.php">page1</a></li>
+        <li><a href="test.php">test1</a></li>
+        <li><a href="test2.php">test2</a></li>
+        <li><a href="test3.php">test3</a></li>
+    </ul>
+</nav>
---
+++ coursPHP/Exo2/test3.php
@@ -0,0 +1,8 @@
+<?php
+    include("common/header.php");
+    include("common/menu1.php");
+?>
+
+<?php
+    include("common/footer.php");
+?>
