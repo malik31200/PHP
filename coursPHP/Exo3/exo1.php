e66ac310e10e83a930ca35f80e08ed747f1f54d9
malik31200 <yka-vtc@outlook.fr>
2025-12-11 14:16:27 +0100

    add button submit with enter value method get

--- coursPHP/Exo3/exo1.php
+++ coursPHP/Exo3/exo1.php
@@ -5,12 +5,19 @@
 ?>
 
 <h1>Affichage des tables de multiplication</h1>
-
+<form action="#" method="get">
+    <label for="table">Table de multiplication : </label>
+    <input type="number" name="table" id="table">
+    <input type="submit" value="Envoyer">
+</form>
 <?php
-    define("TABLE", 8);
-    echo "<h2> La table de ". TABLE ." :" . "</h2>";
+    if(isset($_GET["table"])){
+    echo "<h2> La table de ". $_GET["table"] ." :" . "</h2>";
     for($i = 1; $i<= 10 ; $i++){
-        echo $i . " * ". TABLE . " = ". $i * TABLE ."<br/>";
+        echo $i . " * ". $_GET["table"] . " = ". $i * $_GET["table"] ."<br/>";
+        }
+    } else {
+        echo "<h2> Saisir une valeur dans le champ ci-dessus</h2>";
     }
 ?>
 
