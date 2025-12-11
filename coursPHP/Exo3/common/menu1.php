c74972e5bdafc4ceb6902bbc44d5d585679d54c6
malik31200 <yka-vtc@outlook.fr>
2025-12-11 14:52:52 +0100

    create a pyramide

--- coursPHP/Exo3/common/menu1.php
+++ coursPHP/Exo3/common/menu1.php
@@ -2,5 +2,6 @@
     <ul>
         <li><a href="index.php">Accueil</a></li>
         <li><a href="exo1.php">Excercice 1</a></li>
+        <li><a href="0-pyramide.php">Excercice 2</a></li>
     </ul>
 </nav>
