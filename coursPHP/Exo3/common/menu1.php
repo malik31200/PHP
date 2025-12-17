b48163e823309a94765ae3c4139842260937473b
malik31200 <yka-vtc@outlook.fr>
2025-12-17 17:59:46 +0100

    Creating a dropdown list to choose a male or female character

--- coursPHP/Exo3/common/menu1.php
+++ coursPHP/Exo3/common/menu1.php
@@ -3,5 +3,9 @@
         <li><a href="index.php">Accueil</a></li>
         <li><a href="exo1.php">Excercice 1</a></li>
         <li><a href="0-pyramide.php">Excercice 2</a></li>
+        <li><a href="1-circle.php">Excercice 3</a></li>
+        <li><a href="2-find_the_number.php">Excercice 4</a></li>
+        <li><a href="3-calculate_average.php">Excercice 5</a></li>
+        <li><a href="4-display_character.php">Excercice 6</a></li>
     </ul>
 </nav>
