81db6c85e18b2178128ad68215ded005971381d9
malik31200 <yka-vtc@outlook.fr>
2025-12-23 16:52:43 +0100

    create a class with attributates

---
+++ coursPHP/Exo4_OOP/1-first_class.php
@@ -0,0 +1,30 @@
+<?php
+
+class Personnage {
+    public $nom = "Cloud"; // attributs
+    public $img = "player_h.jpg";
+    public $age = 22;
+    public $sexe = true;
+    public $pm = 80;
+    public $pv = 75;
+}
+    include("common/header.php");
+    include("common/menu1.php");
+?>
+
+<h2>Personnage : </h2>
+
+<?php
+    $p1 = new Personnage();
+    echo "Nom : ". $p1->nom . "<br/>";
+    echo "Age : ". $p1->age . "<br/>";
+    $p2 = new Personnage();
+    $p2->nom = "Yuna";
+    $p2->age = 21;
+    echo "Nom : ". $p2->nom . "<br/>";
+    echo "Age : ". $p2->age . "<br/>";
+?>
+
+<?php
+    include("common/footer.php");
+?>
---
+++ coursPHP/Exo4_OOP/common/menu1.php
@@ -0,0 +1,7 @@
+<nav>
+    <ul>
+        <li><a href="index.php">Accueil</a></li>
+        <li><a href="0-personnage.php">Personnage</a></li>
+        <li><a href="1-first_class.php">Personnage-Class</a></li>
+    </ul>
+</nav>
